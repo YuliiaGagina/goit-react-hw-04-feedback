bc6fc11dd8fc509eaac6e118c914928db0669e8b
YuliiaGagina <ygaginazy@gmail.com>
2023-02-15 23:46:56 +0200

    feedback hook

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,4 +1,5 @@
-import { Component } from 'react';
+
+import { useState } from 'react';
 import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
 // import StatisticListItem from 'components/StatisticListItem/StatisticListItem';
 import { Subtitle } from './Feedback.styled';
@@ -6,45 +7,57 @@ import Statistics from 'components/Statistics/Statistics';
 import Notification from 'components/Notification/Notification';
 import Section from 'components/Section/Section';
 
-export class Feedback extends Component {
-  state = {
+
+
+export function Feedback() {
+  const [points, setPoints] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
-  handelIncrement = value => {
-    this.setState(prevState => ({
+  });
+    
+  
+
+  
+  
+const  handelIncrement = value => {
+    setPoints(prevState => ({
+    ...prevState,
       [value]: prevState[value] + 1,
     }));
   };
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((acc, item) => acc + item, 0);
-  };
+    // this.setState(prevState => ({
+    //   [value]: prevState[value] + 1,
+    // }));
 
-  countPositiveFeedbackPercentage = () => {
-    if (this.countTotalFeedback() === 0) {
-      return 0;
-    } else {
-      return Math.round((this.state.good / this.countTotalFeedback()) * 100);
-    }
-  };
-  render() {
+  const  countTotalFeedback = () => {
+      return Object.values(points).reduce((acc, item) => acc + item, 0);
+    };
+
+    const  countPositiveFeedbackPercentage = () => {
+      if (countTotalFeedback() === 0) {
+        return 0;
+      } else {
+        return Math.round((points.good / countTotalFeedback()) * 100);
+      }
+    };
+    
     return (
       <div>
         <Section title="Please leave feedback">
         
           <FeedbackOptions
-            options={this.state}
-            onLeaveFeedback={this.handelIncrement}
+            options={points}
+            onLeaveFeedback={handelIncrement}
           />
         </Section>
-        {this.countTotalFeedback() > 0 ? (
+        {countTotalFeedback() > 0 ? (
           <>
             <Subtitle>Statistics</Subtitle>
             <Statistics
-              options={this.state}
-              countTotal={this.countTotalFeedback()}
-              countPositiveFeedback={this.countPositiveFeedbackPercentage()}
+              options={points}
+              countTotal={countTotalFeedback()}
+              countPositiveFeedback={countPositiveFeedbackPercentage()}
             />
           </>
         ) : (
@@ -53,12 +66,13 @@ export class Feedback extends Component {
       
       </div>
     );
-  }
 
- 
-
-}
+  };
 
 Feedback.propTypes = {
     
 }
+
+// Feedback.propTypes = {
+    
+// }
